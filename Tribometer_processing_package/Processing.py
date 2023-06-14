7dca321bee0f8bf9ba39182056b010b31753e881
Kevin-Veenboer <135600479+Kevin-Veenboer@users.noreply.github.com>
2023-06-14 10:18:00 +0200

    Path of processing file
    There were still some error in the processing file. They arose due to the fact that the code was copied from the prototype and some of the code was specifically tailored to the prototype environment.

--- Tribometer_processing_package/Processing.py
+++ Tribometer_processing_package/Processing.py
@@ -1,13 +1,11 @@
+import os
+import config
+from os import listdir 
+
 assert os.path.exists("./config.py"), "config file is missing, please include the config.py file in the working folder"
 assert os.path.exists(config.RAW_PATH), "make sure that the folder with the raw data exists"
 assert len(listdir(config.RAW_PATH)) == 1, "make sure that the folder with the raw data contians a single raw dataset"
 
-from os import listdir 
-import os
-import numpy as np
-import pandas as pd 
-import matplotlib.pyplot as plt
-import config
 
 #Initialize and check folder contents/structure
 if not os.path.exists(config.RAW_PATH):
@@ -17,11 +15,11 @@ if not os.path.exists(config.CLEAN_PATH):
     
     
 #Clean up old files
-for file in listdir(config.CLEAN_PATH)[1:]:
+for file in listdir(config.CLEAN_PATH)[0:]:
     os.remove(config.CLEAN_PATH + file)
    
 #Start with cleaning up RAW data and putting it in a temporary file
-file = config.RAW_PATH + listdir(config.RAW_PATH)[1]    
+file = config.RAW_PATH + listdir(config.RAW_PATH)[0]    
 with open(file, 'r') as data:
     with open(config.CLEAN_PATH+ "temp.csv",'w') as output:
         skipping = True
@@ -98,3 +96,4 @@ with open(config.CLEAN_PATH+ "temp.csv", 'r') as data:
             file.write(line)
             
 #removing the temporary file
+os.remove(config.CLEAN_PATH+ "temp.csv")
