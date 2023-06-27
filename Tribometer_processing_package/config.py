5c2e3db6ad629ad898d5f59d3618179dc05eb230
Kevin-Veenboer <135600479+Kevin-Veenboer@users.noreply.github.com>
2023-06-27 13:29:03 +0200

    Delete Tribometer_processing_package directory

--- Tribometer_processing_package/config.py
+++
@@ -1,5 +0,0 @@
-RAW_PATH = "C:/Users/Kevin/Desktop/NS project/Files/Tribometer/DATA/RAW/"
-CLEAN_PATH = "C:/Users/Kevin/Desktop/NS project/Files/Tribometer/DATA/CSV/"
-
-#currently does nothing
-REVERSE= True
